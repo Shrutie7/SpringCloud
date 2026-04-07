6653b56ccbd7275b0cd977676091ebf5054636ad
Steak <lakhotiya.sil07@gmail.com>
2026-04-08 03:35:04 +0530

    Added initial code for payment service

---
+++ payment-service/src/main/java/com/shrucode/payment_service/service/PaymentService.java
@@ -0,0 +1,27 @@
+package com.shrucode.payment_service.service;
+
+import com.shrucode.payment_service.entity.Payment;
+import com.shrucode.payment_service.repository.PaymentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Random;
+import java.util.UUID;
+
+@Service
+public class PaymentService {
+
+    @Autowired
+    private PaymentRepository paymentRepository;
+
+    public Payment doPayment(Payment payment){
+        payment.setTransactionId(UUID.randomUUID().toString());
+        payment.setPaymentStatus(fetchPaymentStatus());
+        return paymentRepository.save(payment);
+    }
+
+    public String fetchPaymentStatus(){
+        //now its random but this api call happens from 3rd party payment gateway(paypal/phonepe/gpay)
+        return new Random().nextBoolean()?"sucess":"failure";
+    }
+}
