f3d2999bc43c05a48c98d98ea7f651ecb26f0889
Steak <lakhotiya.sil07@gmail.com>
2026-04-11 08:03:48 +0530

    Added feign client in order service to connect with inventory service

--- inventory-service/src/main/java/com/shrucode/inventory_service/service/InventoryService.java
+++ inventory-service/src/main/java/com/shrucode/inventory_service/service/InventoryService.java
@@ -24,6 +24,7 @@ public class InventoryService {
         InventoryReduceResponse inventoryReduceResponse = new InventoryReduceResponse();
 //        Inventory inventory = new Inventory(); // will create a new row if i create a new object of entity we need to update existing row
 
+        //use redis cache here for db call -- ttl...........
         Inventory byProductId = inventoryRepository.findByProductId(inventoryReduceRequest.getProductId());//db call ek bar krna pura object inventory ka yhi miljaiga
         if(byProductId==null ){
             inventoryReduceResponse.setMessage("Product Not found!");
